8e089a925c3c0d678b977658f3dd08f7ef143fba
Šárka F. <sarka.flidrova@email.cz>
2025-04-15 14:10:19 +0200

    Add a counter component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, signal, Signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GreetingComponent],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
