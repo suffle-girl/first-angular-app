80cebfd647353a7c3cd0f498cdff81ce883ea9da
Šárka F. <sarka.flidrova@email.cz>
2025-04-15 13:08:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+  `,
+  styles: [
+    `
+      main {
+        padding-inline: 16px;
+      }
+    `,
+  ],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
